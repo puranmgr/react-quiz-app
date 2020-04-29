69daee9f23ae22c943bb0bc52d86dd4e5035afcd
puranmgr <puranmgr@gmail.com>
2020-04-29 15:19:43 +0100

    Updates

---
+++ precache-manifest.a259fc8e9c2821dfc17b7f3d23aaf1cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68aa106fffb7022a353d588585e37e0c",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "cff44b4b47ceef8e1dfd",
+    "url": "/react-quiz-app/static/css/main.f474f262.chunk.css"
+  },
+  {
+    "revision": "3eef8325ebc6f4cc23ea",
+    "url": "/react-quiz-app/static/js/2.f5ded180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.f5ded180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff44b4b47ceef8e1dfd",
+    "url": "/react-quiz-app/static/js/main.3504a210.chunk.js"
+  },
+  {
+    "revision": "bdb1df006bd7c5e32027",
+    "url": "/react-quiz-app/static/js/runtime-main.d531f481.js"
+  }
+]);
